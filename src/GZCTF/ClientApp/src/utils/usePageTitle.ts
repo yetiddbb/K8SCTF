97e0807de4293c87d7e09bceef2bace492087109
yetiddbb <yetiddbb@163.com>
2023-05-05 13:10:53 +0800

    Update usePageTitle.ts

--- src/GZCTF/ClientApp/src/utils/usePageTitle.ts
+++ src/GZCTF/ClientApp/src/utils/usePageTitle.ts
@@ -4,7 +4,7 @@ import { useConfig } from './useConfig'
 export const usePageTitle = (title?: string) => {
   const { config, error } = useConfig()
 
-  const platform = error ? '砺剑::实战演练平台' : `${config?.title ?? 'GZ'}::CTF`
+  const platform = error ? '砺剑::实战演练平台' : `砺剑::实战演练平台`
 
   useDocumentTitle(
     typeof title === 'string' && title.trim().length > 0 ? `${title} - ${platform}` : platform
